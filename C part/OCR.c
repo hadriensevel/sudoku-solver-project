cecda4d9cb0c86260e07a5f8c2bbb624ff51157b
Hadrien <hadriensevel@outlook.com>
2021-05-01 23:26:26 +0200

    Update

--- C part/OCR.c
+++ C part/OCR.c
@@ -8,7 +8,8 @@
 
 int readCell(uint8_t *cell, const char *cellPath);
 int isCellEmpty(uint8_t *cell, int seuil);
-int isNumber(uint8_t *cell, int chiffre, int pixelDeDepart);
+void findNumber(uint8_t *cell, float *ratios);
+float getRatioCommonPixels(uint8_t *cell, int chiffre, int startPixel);
 uint8_t getPixelBitmap(int chiffre, int ligne, int colonne);
 
 int main(int argc, const char *argv[]) {
@@ -17,9 +18,13 @@ int main(int argc, const char *argv[]) {
     return 0;
   } */
 
-  const char cellPath[100] = "Bitmaps\\bbox_empty.bin"; // TODO: remplacer par argument
+  const char cellPath[100] = "D:\\Documents\\EPFL\\BA4-1\\Programmation\\sudoku-solver-project\\C part\\Bitmaps\\bbox_5.bin"; // TODO: remplacer par argument
 
+  // Créer le tableau qui va contenir les pixels du fichier
   uint8_t *cell = malloc(CELLSIZE * CELLSIZE * sizeof(uint8_t));
+
+  // Créer le tableau qui va contenir le ratio de similitude le plus élévé pour chaque chiffre
+  float *ratios = malloc(9 * sizeof(float));
 
   // Lit le fichier binaire, enregistre les pixels dans le tableau cell et continue si pas d'erreur
   if (!readCell(cell, cellPath)) {
@@ -30,15 +35,19 @@ int main(int argc, const char *argv[]) {
     }
 
     else {
-      for (int i = 0; i < 9; i++) {
-
-      }
+      findNumber(cell, ratios);
     }
 
   };
 
+  for (int i = 0; i < 9; i++) {
+    printf("%d: %0.4f %%\n", i+1, ratios[i]);
+  }
+
   free(cell);
+  free(ratios);
   cell = NULL;
+  ratios = NULL;
 
   return 0;
 }
@@ -110,4 +119,58 @@ int isCellEmpty(uint8_t *cell, int seuil) {
 
   // Si le pourcentage de pixels blancs est suprérieur au seuil donné retourne 1 sinon 0
    return (nombrePixelsBlancs * 100 / nombrePixels >= seuil) ? 1 : 0;
+}
+
+float getRatioCommonPixels(uint8_t *cell, int chiffre, int startPixel) {
+
+  // Déclaration des variables
+  int nombrePixelsCommuns = 0;
+
+  // Boucle sur les lignes du bitmap
+  for (int i = 0; i < 24; i++) {
+
+    //Boucles sur les colonnes du bitmap
+    for (int j = 0; j < 16; j++) {
+
+      if (getPixelBitmap(chiffre, i, j) == cell[j + startPixel + CELLSIZE * i]) nombrePixelsCommuns++;
+
+    }
+  }
+
+  // Retourne le ratio de pixels communs
+  return nombrePixelsCommuns * 100.0 / (24.0 * 16.0);
+}
+
+void findNumber(uint8_t *cell, float *ratios) {
+
+  // Boucle sur les 9 chiffres
+  for (int i = 1; i <= 9; i++) {
+
+    float temp = 0;
+    ratios[i] = 0;
+
+    // Boucle sur les 560 déplacements que le bitmap doit effectuer
+    for (int j = 0; j < 560; j++) {
+
+      // Détermine la ligne et la colonne du pixel de départ de la cellule
+      // 28 -> 28 déplacements en largeur car 44 (taille cellule) - 16 (taille bitmap) = 28
+      int ligneStartPixel = (j - (j % 28)) / 28;
+      int colonneStartPixel = j % 28;
+
+      // Calcul le pixel de départ pour le tableau des pixels de la cellule
+      int startPixel = 44 * ligneStartPixel + colonneStartPixel;
+
+      // Calcul le ratio de pixels communs pour la position courante du bitmap sur la cellule
+      temp = getRatioCommonPixels(cell, i, startPixel);
+
+      // Si le ratio obtenu est plus grand que précédent, l'enregistre dans le tableau des ratios
+      if (temp > ratios[i-1]) {
+        ratios[i-1] = temp;
+        printf("Chiffre: %d, ligne: %d, colonne: %d\n", i, ligneStartPixel, colonneStartPixel);
+        }
+
+    }
+  }
+
+  return;
 }
